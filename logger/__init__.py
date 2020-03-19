08ebc036df33d4a067548acda3be14fda5f8a9f4
Sunil <Sunhick@gmail.com>
2020-03-18 20:14:19 -0700

    Create __init__.py

---
+++ logger/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+__init__.py
+
+Exports the colored logs module
+
+"""
+
+from .ChromeConstants import ChromeConstants
+from .ChromeColoredLogger import ChromeColoredLogger
+from .ChromeLogLine import ChromeLogLine
