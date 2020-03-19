02bb4f21baf7920c7a25e0dd3cc9813c436c0147
Sunil <Sunhick@gmail.com>
2020-03-18 21:54:28 -0700

    use chrome log line

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from typing_extensions import Final
 
 from logger import *
 
+coloredLog: ChromeColoredLogger = ChromeColoredLogger()
+
 def quit(signum, frame) -> None:
     # TODO: might need threading.
     sys.exit(0)
@@ -24,14 +26,20 @@ def quit(signum, frame) -> None:
 def installSigTermHandler() -> None:
     signal.signal(signal.SIGTERM, quit)
 
+def processLine(uncoloredLine: str) -> None:
+    try:
+        chromeLogLine: ChromeLogLine = ChromeLogLine(uncoloredLine)
+        coloredLog.log(chromeLogLine)
+    except Exception as e:
+        # log line doesn't follow standard chrome convention
+        # dump it as it is to the console "unformatted"
+        print(e.args)
+
 def main(args: List[str]) -> None:
     installSigTermHandler()
-    colorize: ChromeColoredLogger = ChromeColoredLogger()
 
     for rawLine in sys.stdin:
-        uncoloredLine: str = rawLine.strip()
-        chromeLogLine: ChromeLogLine = ChromeLogLine(uncoloredLine)
-        print(uncoloredLine)
+        processLine(rawLine.strip())
 
 if __name__ == "__main__":
     main(sys.argv[1:])
