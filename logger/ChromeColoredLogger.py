2df716724ffe4bca5126e55d5deec0144c823379
Sunil <Sunhick@gmail.com>
2020-03-18 21:53:40 -0700

    format chrome log lines

--- logger/ChromeColoredLogger.py
+++ logger/ChromeColoredLogger.py
@@ -7,6 +7,42 @@ Colorize chrome logs for the terminal
 
 """
 
+from .ChromeLogLine import ChromeLogLine
+
+from typing import (Callable,
+                    Dict,
+                    DefaultDict)
+
+from colored import (fg,
+                     bg,
+                     attr)
+
 class ChromeColoredLogger(object):
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.fmtPicker: Dict[str, Callable[[ChromeLogLine], None]] = {
+            "INFO": self.info,
+            "WARNING": self.warning,
+            "ERROR": self.error,
+            "FATAL": self.fatal,
+        }
+
+    def log(self, chromeLogLine: ChromeLogLine) -> None:
+        if (chromeLogLine.logLevel in self.fmtPicker):
+            self.fmtPicker[chromeLogLine.logLevel](chromeLogLine)
+        else:
+            self.unknown(chromeLogLine)
+
+    def info(self, logLine: ChromeLogLine) -> None:
+        print(f'{fg("green")} {logLine} {attr("reset")}')
+
+    def warning(self, logLine: ChromeLogLine) -> None:
+        print(f'{fg("yellow")} {logLine} {attr("reset")}')
+
+    def error(self, logLine: ChromeLogLine) -> None:
+        print(f'{fg("red")} {logLine} {attr("reset")}')
+
+    def fatal(self, logLine: ChromeLogLine) -> None:
+        print(f'{fg("red")} {logLine} {attr("reset")}')
+
+    def unknown(self, logLine: ChromeLogLine) -> None:
+        print(f'{fg("blue")} {logLine} {attr("reset")}')
